21d72f2a4389d7d05d817dea862310663e3ae317
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-19 09:16:57 +0100

    Finished main functionality of bot

--- main.py
+++ main.py
@@ -23,13 +23,37 @@ password_field.send_keys(password)
 password_field.send_keys(Keys.ENTER)
 time.sleep(5)
 
-phone_field = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
-if phone_field == "":
-    phone_field.send_keys(phone_number)
-submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
-submit_button.click()
+all_listings = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
+for listing in all_listings:
+    print("called")
+    listing.click()
+    time.sleep(2)
+    try:
+        apply_button = driver.find_element(By.CSS_SELECTOR, ".jobs-s-apply button")
+        apply_button.click()
+        time.sleep(5)
+        phone_field = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
+        if phone_field == "":
+            phone_field.send_keys(phone_number)
+        submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+            close_button.click()
+            time.sleep(2)
+            discard_button = driver.find_elements(By.CLASS_NAME, "artdeco-modal__confirm-dialog-btn")[1]
+            discard_button.click()
+            print("Complex application, skipped.")
+            continue
+        else:
+            submit_button.click()
+    except NoSuchElementException:
+        print("No application button, skipped.")
+        continue
+
+time.sleep(5)
 
 driver.quit()
+
 #news_tags = soup.findAll("a", href=True)
 #news = [tag.getText() for tag in news_tags]
 
